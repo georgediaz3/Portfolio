61c5678ebc2f162c8181a318d45c44b49840b2d1
georgediaz3 <georgezdiaz@gmail.com>
2024-12-07 12:03:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
+
+
 gsap.registerPlugin(CustomEase);
 
 gsap.set(".nav",{display:"none"})
